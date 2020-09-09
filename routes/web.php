9c0acc4eaa0ea45115b992fe4ceac8b3cd6d6a9e
BrilyanNathanael <brilyannaathan@gmail.com>
2020-09-09 10:30:40 +0700

    added middleware auth

--- routes/web.php
+++ routes/web.php
@@ -14,15 +14,15 @@ use Illuminate\Support\Facades\Route;
 */
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
-
-Route::get('/', 'ArticleController@index');
-Route::get('/menambahkan', 'ArticleController@create');
-Route::post('/menambahkan', 'ArticleController@store');
-Route::get('/logout','Auth\LoginController@logout');
-Route::get('/view/{id}', 'ArticleController@show');
-Route::get('/mengubah/{id}', 'ArticleController@edit');
-Route::patch('/mengubah/{id}', 'ArticleController@update');
-Route::delete('/menghapus/{id}', 'ArticleController@destroy');
+Route::group(['middleware' => ['auth']],function(){
+    Route::get('/', 'ArticleController@index');
+    Route::get('/menambahkan', 'ArticleController@create');
+    Route::post('/menambahkan', 'ArticleController@store');
+    Route::get('/logout','Auth\LoginController@logout');
+    Route::get('/view/{id}', 'ArticleController@show');
+    Route::get('/mengubah/{id}', 'ArticleController@edit');
+    Route::patch('/mengubah/{id}', 'ArticleController@update');
+    Route::delete('/menghapus/{id}', 'ArticleController@destroy');
+});
 
 
